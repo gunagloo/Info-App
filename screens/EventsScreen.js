3d3c55c48416cf445344b157a8cba983273e9de8
unknown <zheng_guangwei@students.edu.sg>
2022-06-07 10:07:24 +0800

    added second events page

--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import { Text, View, Button, StyleSheet} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
 
-function EventsScreen ( {navigation} ) {
+function EventsScreen () {
+    const navigation = useNavigation()
     return (
-        <View style = {{ flex: 1, justifyContent: 'center', 
-        alignItems: 'center', backgroundColor: 'lightgreen' }}>
+        <View style = {{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'gray' }}>
             <Text>Events First Page!</Text>
+            <Button onPress= {() => navigation.navigate("EventsSecondScreen")} title="Second Screen"/>
+        </View>
+    );
+}
+
+function EventsSecondScreen() {
+    return (
+        <View style = {{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'gray'}}>
+            <Text>Events Second Page!</Text>
         </View>
     );
 }
@@ -17,6 +27,7 @@ export default function EventsStack() {
     return (
         <Stack.Navigator>
             <Stack.Screen name = "EventsHome" component = {EventsScreen} />
+            <Stack.Screen name = "EventsSecondScreen" component = {EventsSecondScreen} />
         </Stack.Navigator>
     )
 }
